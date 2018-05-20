948e2ff2139633ab2578796f75de074e15644227
Ricardo Hui <eehaha@gmail.com>
2018-05-20 15:04:47 +0800

    add a new line at the end of file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 /**
  * This is a class.
  */
@@ -12,8 +13,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param someone the name of a person
+  * @return greeting string
+  */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
